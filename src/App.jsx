fb8701170929f93c7723eea6b295f76affe374fe
Alvarobyui <u701860138@byui.edu>
2023-06-15 22:31:58 -0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import data2 from "../stays.json";
-
-/* function Card(props) {
-  return (
-    <section key={props.i}>
-      <h1 key={i}>{el.city}</h1>
-      <img src={props.src} alt="photo" />
-    </section>
-  )
-} */
-
 
 function App() {
   // La variable data es la que va a almacenar los datos de "stays.json" y setData nos ayudará a guardar esos datos en esa variable. Es necesario que inicialicemos esa variable como un array vacío para evitar errores.
@@ -22,23 +11,35 @@ function App() {
     try {
       const res = await fetch("stays.json");
       const resJson = await res.json();
-      // Aquí guardamos los datos de "stays.json" en la variable data.
+
       setData(resJson);
     } catch (error) {
       console.log(error);
     }
   };
 
-  // Este Hook te va a ejecutar la función getData cada vez que la página se renderice.
+
   useEffect(() => {
     getData();
   }, []);
 
-  // Puedes ver la variable data en consola.
-  console.log(data);
-  console.log(data2);
   
+  console.log(data);
 
+  
+  return (
+    <Card />
+  )
+
+  function Card() {
+    return(
+    <>
+      {data.map((el, i) => {
+        return <h1 key={i}>{el.city}</h1>;
+      })}
+    </>
+    );
+  }
 }
 
 export default App;
